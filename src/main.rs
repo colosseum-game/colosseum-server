fba2569784a46c50fe90fdb6f4edf7c51750e7ab
Chay Nabors <chaynabors@gmail.com>
2020-11-28 23:51:53 -0600

    rewrote to perform simple async connection accepting

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,30 @@
-use colosseum::{
-    combatant::Combatant,
-    message::{
-        MessageToClient,
-        MessageToServer,
-    },
-    party::{
-        CONSUMABLES_INVENTORY_SIZE,
-        Party
-    },
+use tokio::{
+    net::TcpListener,
+    sync::mpsc,
 };
+use tokio::runtime;
 
-use std::{
-    io::{
-        Read,
-        Write,
-    },
-    net::{
-        Ipv4Addr,
-        SocketAddrV4,
-        TcpListener,
-        TcpStream
-    },
-};
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let runtime = runtime::Builder::new_multi_thread()
+        .enable_io()
+        .build()
+        .unwrap();
 
-fn main() -> std::io::Result<()> {
-    // bind to address and port
-    let addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 40004);
-    let listener = TcpListener::bind(&addr)?;
+    let listener = runtime.block_on(TcpListener::bind("localhost:40004")).unwrap();
+    let (tx, mut rx) = mpsc::channel(16);
 
-    // imitate database retrieval
-    let brayden: Combatant = serde_json::from_slice(include_bytes!("../combatants/brayden.json"))?;
-    let chay: Combatant = serde_json::from_slice(include_bytes!("../combatants/chay.json"))?;
+    runtime.spawn(async move {
+        loop {
+            let client = listener.accept().await.expect("failed to accept client");
+            tx.send(client).await.expect("failed to return an incoming client");
+        }
+    });
 
-    // the initial gamestate
-    let gamestate = MessageToClient::GameState([
-        Party {
-            members: [Some(brayden), None, None, None],
-            consumables_inventory: [None; CONSUMABLES_INVENTORY_SIZE],
-        },
-        Party {
-            members: [Some(chay), None, None, None],
-            consumables_inventory: [None; CONSUMABLES_INVENTORY_SIZE],
-        },
-    ]);
-
-    let mut players: Vec<TcpStream> = vec![];
-    while players.len() < 2 {
-        if let Ok((mut stream, addr)) = listener.accept() {
-            println!("connection from {}", addr);
-            stream.write(&serde_mp::to_vec(&gamestate).unwrap())?;
-            players.push(stream);
+    let mut clients = vec![];
+    loop {
+        while let Ok((client_stream, client_addr)) = rx.try_recv() {
+            clients.push(client_stream);
+            println!("client connected from: {}", client_addr);
         }
     }
-
-    Ok(())
 }
