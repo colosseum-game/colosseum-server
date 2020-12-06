133e465080d16d4c227fd0ac16bf6613c971a724
Chay Nabors <chaynabors@gmail.com>
2020-12-05 20:01:47 -0600

    send a message to client

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use colosseum::{
+    combat_state::CombatState,
+    connection::Connection,
+    message::Message,
+};
+
 use tokio::{
     net::TcpListener,
-    sync::mpsc,
+    runtime,
+    sync::{
+        mpsc,
+    },
 };
-use tokio::runtime;
+
+struct Match {
+    clients: Vec<Connection>,
+    combat_state: CombatState,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let runtime = runtime::Builder::new_multi_thread()
@@ -10,7 +23,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build()
         .unwrap();
 
-    let listener = runtime.block_on(TcpListener::bind("localhost:40004")).unwrap();
+    let listener = runtime.block_on(TcpListener::bind("127.0.0.1:40004"))?;
     let (tx, mut rx) = mpsc::channel(16);
 
     runtime.spawn(async move {
@@ -21,10 +34,37 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     });
 
     let mut clients = vec![];
+    let mut matches = vec![];
     loop {
-        while let Ok((client_stream, client_addr)) = rx.try_recv() {
-            clients.push(client_stream);
-            println!("client connected from: {}", client_addr);
+        while let Ok((stream, addr)) = rx.try_recv() {
+            println!("client connected from: {}", addr);
+            clients.push(Connection::new(addr, stream));
+
+            let combat_state = CombatState::new();
+            let message = Message::CombatState(combat_state);
+            runtime.block_on(async { clients[0].write_message(&message).await })?;
+        }
+
+        while clients.len() >= 2 {
+            let mut p1 = clients.pop().unwrap();
+            let mut p2 = clients.pop().unwrap();
+
+            let mut combat_state = CombatState::new();
+
+            let message = Message::CombatState(combat_state);
+            runtime.block_on(async { p1.write_message(&message).await })?;
+            runtime.block_on(async { p2.write_message(&message).await })?;
+            combat_state = if let Message::CombatState(combat_state) = message { combat_state } else { unreachable!() };
+
+            println!("started a match: {}, {}", p1.addr, p2.addr);
+            matches.push(Match {
+                clients: vec![p1, p2],
+                combat_state: combat_state,
+            });
+        }
+
+        for i in 0..matches.len() {
+            runtime.block_on(async { matches[i].clients[0].read_message().await })?;
         }
     }
 }
